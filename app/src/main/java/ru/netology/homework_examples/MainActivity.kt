04685fa9dc986f3d095e259c34b243c2153b1457
PavelBelonogov85 <belonogovp@mail.ru>
2022-11-29 10:50:11 +0300

    change text by button

--- app/src/main/java/ru/netology/homework_examples/MainActivity.kt
+++ app/src/main/java/ru/netology/homework_examples/MainActivity.kt
@@ -1,11 +1,29 @@
 package ru.netology.homework_examples
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var textsCounter = 0
+        findViewById<Button>(R.id.buttonId).setOnClickListener {
+            textsCounter = if (textsCounter==3) {0} else {textsCounter+1}
+            println("Хотим поменять текст на R.string.text1"+textsCounter)
+            val myTextView = findViewById<View>(R.id.textViewId) as TextView
+
+
+            myTextView.setText(resources.getString(R.string.text1))
+            //val newText = resources.getString(R.string["text"+textsCounter])  // как взять ресурс по имени ???
+            //myTextView.setText(newText)
+            myTextView.text = resources.getString(R.string.text2)
+        }
+
     }
 }
